c3016ae4de865ee420795dcfcc26af1892eca43b
MOHAMMAD-RASHIM <mohammadrashimkallaje@gmail.com>
2026-02-24 17:50:13 +0530

    just added

--- src/com/tap/loops/FactorsOfN.java
+++ src/com/tap/loops/FactorsOfN.java
@@ -13,9 +13,17 @@ public class FactorsOfN {
 				System.out.print(i+" ");
 			}
 		}
-		
-		
-		
+		System.out.println();
+		// to get the number of factors to the above logic use count variable
+		int count=0;
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
 		scanner.close();
 		
 	}
