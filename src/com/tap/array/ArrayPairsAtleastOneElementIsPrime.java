b78d9a26dcfc3b3c2afa0ffda7023426cb9ad444
MOHAMMAD-RASHIM <mohammadrashimkallaje@gmail.com>
2026-03-01 10:48:51 +0530

    just added

--- src/com/tap/array/ArrayPairsAtleastOneElementIsPrime.java
+++ src/com/tap/array/ArrayPairsAtleastOneElementIsPrime.java
@@ -21,7 +21,7 @@ public class ArrayPairsAtleastOneElementIsPrime {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-         int[] arr = new int[n];
+        int[] arr = new int[n];
         for(int i=0;i<arr.length;i++)
         {
             arr[i] = scanner.nextInt();
