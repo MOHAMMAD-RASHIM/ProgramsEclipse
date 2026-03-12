443c312d82d6f2cc10670ef5b6af52577771b58c
MOHAMMAD-RASHIM <mohammadrashimkallaje@gmail.com>
2026-03-12 18:09:52 +0530

    add Character Count

---
+++ src/com/tap/strings/CharacterCount.java
@@ -0,0 +1,19 @@
+package com.tap.strings;
+import java.util.*;
+public class CharacterCount {
+	public static int charCount(String str)
+	{
+		return str.length();
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		int result = charCount(str);
+		System.out.println(result);
+		scanner.close();
+		
+		
+	}
+
+}
