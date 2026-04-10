dbdca3568c861db558410bbc82b40724226baa35
MOHAMMAD-RASHIM <mohammadrashimkallaje@gmail.com>
2026-04-10 08:29:33 +0530

    add set problem print elements once if repeats

---
+++ src/com/tap/sets/SetProblem.java
@@ -0,0 +1,30 @@
+package com.tap.sets;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class SetProblem {
+    public static LinkedHashSet<Integer> filterUniqueIntegers(int[] arr)
+    {
+        LinkedHashSet<Integer> set = new LinkedHashSet<Integer>();
+        for(int i=0;i<arr.length;i++)
+        {
+            set.add(arr[i]);
+        }
+        return set;
+    }
+    public static void main(String[] args) {
+        // Your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i] = scanner.nextInt();
+        }
+        LinkedHashSet<Integer> set = filterUniqueIntegers(arr);
+        for(Integer value:set)
+        {
+            System.out.print(value+" ");
+        }
+    }
+}
