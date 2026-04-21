3ed409411089763ec39853e253421e51952bf969
MOHAMMAD-RASHIM <mohammadrashimkallaje@gmail.com>
2026-04-21 21:51:46 +0530

    add Prime sum digits program

---
+++ src/com/tap/loops/PrimeSumDigits.java
@@ -0,0 +1,35 @@
+package com.tap.loops;
+import java.util.Scanner;
+
+class PrimeSumDigits {
+    public static boolean isPrime(int num)
+    {
+        if (num<=1) 
+            return false;
+        for(int i=2;i*i<=num;i++){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void primeSumDigits(int n){
+        int sum = 0;
+        while(n!=0){
+            int lastDigit = n%10;
+            sum+=lastDigit;
+            n/=10;
+        }
+        if(isPrime(sum)){
+            System.out.print("Prime");
+        }else{
+            System.out.print("Not Prime");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Your code here
+        int n = scanner.nextInt();
+        primeSumDigits(n);
+    }
+}
