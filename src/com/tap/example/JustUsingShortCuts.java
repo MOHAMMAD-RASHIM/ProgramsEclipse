339a06e00fdb3146ca80790fe27588ab7b87464a
MOHAMMAD-RASHIM <mohammadrashimkallaje@gmail.com>
2026-09-07 21:56:56 +0530

    add example program

---
+++ src/com/tap/example/JustUsingShortCuts.java
@@ -0,0 +1,17 @@
+package com.tap.example;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class JustUsingShortCuts {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] arr = new int[n];
+		
+		float var1 = scanner.nextFloat();
+		HashMap<Integer, Integer> hashMap = new HashMap<Integer,Integer>();
+	}
+
+}
